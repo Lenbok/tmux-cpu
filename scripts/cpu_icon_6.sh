077a0f97bf4bf27ba9ca76d6b8cdad15c7c2317e
Len Trigg <len@realtimegenomics.com>
2020-10-08 11:29:18 +1300

    Allow 0 level as configurable

--- scripts/cpu_icon_6.sh
+++ scripts/cpu_icon_6.sh
@@ -5,6 +5,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 # script global variables
+cpu_icon_0=""
 cpu_icon_1=""
 cpu_icon_2=""
 cpu_icon_3=""
@@ -12,6 +13,7 @@ cpu_icon_4=""
 cpu_icon_5=""
 cpu_icon_6=""
 
+cpu_icon_0_default=" "
 cpu_icon_1_default="#[fg=colour226]▁"
 cpu_icon_2_default="#[fg=colour220]▂"
 cpu_icon_3_default="#[fg=colour214]▃"
@@ -21,6 +23,7 @@ cpu_icon_6_default="#[fg=colour196]▇"
 
 # icons are set as script global variables
 get_icon_settings() {
+  cpu_icon_0=$(get_tmux_option "@cpu_icon_0" "$cpu_icon_0_default")
   cpu_icon_1=$(get_tmux_option "@cpu_icon_1" "$cpu_icon_1_default")
   cpu_icon_2=$(get_tmux_option "@cpu_icon_2" "$cpu_icon_2_default")
   cpu_icon_3=$(get_tmux_option "@cpu_icon_3" "$cpu_icon_3_default")
@@ -41,16 +44,15 @@ print_icon() {
     echo "$cpu_icon_3"
   elif fcomp 10 $percentage; then
     echo "$cpu_icon_2"
-  elif fcomp 10 $percentage; then
+  elif fcomp 5 $percentage; then
     echo "$cpu_icon_1"
   else
-    echo " "
+    echo "$cpu_icon_0"
   fi
 }
 
 main() {
   get_icon_settings
-  local cpu_icon=$(print_icon "$1")
-  echo "$cpu_icon"
+  print_icon
 }
 main
